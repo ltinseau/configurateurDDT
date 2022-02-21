800e472b619da3ca43c0c083f92b57652ec9afef
Laurent Tinseau <laurent.tinseau@sfr.fr>
2022-02-22 00:03:17 +0100

    ajout headband

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # configurateurDDT
+
 Configurateur de dossier de diagnostics techniques immobiliers
+
+Cet outil détermine la liste des diagnostics à produire pour vendre ou louer un bien immobilier, en prenant compte des documents déjà disponibles.
+
+Reste à faire:
+
+- tout !
---
+++ script.js
@@ -0,0 +1,37 @@
+function navHeadBandDisplay() {
+  const formHeadBand = document.getElementById("formHeadBand");
+  const navButtons = document.querySelectorAll("formHeadBand + label");
+  const navStep = [];
+  const radioStep = [];
+
+  //let tab = [0, 0, 0, 1, 0, 0, 0, 0];
+
+  for (i = 0; i <= 7; i++) {
+    formHeadBand.innerHTML += `<input type="radio" id="step${i}" name="steps"></input>
+  <label for="step${i}" id="label${i}" class="labels">étape ${i}</label>`;
+    navStep[i] = document.getElementById(`label${i}`);
+    console.log("navStep" + i + " = " + navStep[i]);
+    radioStep[i] = document.getElementById(`step${i}`);
+    // console.log("radioStep" + i + " = " + radioStep[i]);
+
+    if (i == 0) {
+      navStep[i].classList.add("firstLabel");
+      navStep[i].textContent = "Introduction";
+    } else if (i == 7) {
+      navStep[i].classList.add("lastLabel");
+      navStep[i].textContent = "Résultat";
+    } else {
+      navStep[i].classList.add("otherLabel");
+    }
+    console.log(formHeadBand);
+
+    formHeadBand.addEventListener("click", (e) => {
+      let toto = e.target.id;
+      console.log(toto);
+    });
+  }
+}
+
+window.addEventListener("load", () => {
+  navHeadBandDisplay();
+});
